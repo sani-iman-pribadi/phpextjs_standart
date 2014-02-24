5310f0d211ad172cf81f6726c49b0b959225bce6
Sani Iman Pribadi <surat.ajaib@gmail.com>
2014-02-24 12:30:36 +0700

    Yii UserIdentity Update

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -1,40 +1,42 @@
 <?php
-
-    class UserIdentity extends CUserIdentity {
-
-        private $_id;
-
-        public function authenticate() {
-            $record = User::model()->findByAttributes(array('username' => $this->username));
-            if ($record === null)
-                $this->errorCode = self::ERROR_USERNAME_INVALID;
-            else if ($record->password !== md5($this->password))
-                $this->errorCode = self::ERROR_PASSWORD_INVALID;
-            else {
-                $this->_id = $record->id;
-                Yii::app()->session['user'] = $this->getUser();
-                $this->setState('title', $record->name);
-                $this->errorCode = self::ERROR_NONE;
-            }
-            return !$this->errorCode;
+class UserIdentity extends CUserIdentity
+{
+    private $_id;
+    public function authenticate()
+    {
+        $record=User::model()->findByAttributes(array('username'=>$this->username));
+        if($record===null)
+            $this->errorCode=self::ERROR_USERNAME_INVALID;
+        else if($record->password!==md5($this->password))
+            $this->errorCode=self::ERROR_PASSWORD_INVALID;
+        else
+        {   
+            $this->_id=$record->id;
+            Yii::app()->session['user'] = $this->getUser();
+            $this->setState('title', $record->name);
+            $this->errorCode=self::ERROR_NONE;
         }
-
-        public function getId() {
-            return $this->_id;
-        }
-
-        public function getData() {
-            $user = User::model()->findByPk($this->_id);
-            return $user;
-        }
-
-        protected function loadUser($id = null) {
-            if ($this->_model === null) {
-                if ($id !== null)
-                    $this->_model = User::model()->findByPk($id);
-            }
-            return $this->_model;
-        }
-
+        return !$this->errorCode;
+    }
+ 
+    public function getId()
+    {
+        return $this->_id;
     }
     
+    public function getUser(){
+        $userId = $this->getId();
+        $user = CJSON::encode(User::model()->findByPk($userId));
+        return $user;
+    }
+  
+    protected function loadUser($id=null)
+    {
+        if($this->_model===null)
+        {
+            if($id!==null)
+                $this->_model=User::model()->findByPk($id);
+        }
+        return $this->_model;
+    }
+}
