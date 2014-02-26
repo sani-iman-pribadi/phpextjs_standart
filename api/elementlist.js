f51f4afe30c0ae5d2ead1b0b6047ae967679947e
Sani Iman Pribadi <surat.ajaib@gmail.com>
2014-02-26 16:36:53 +0700

    Canceling PHP API Docs

--- api/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","ContactForm"],["c","Controller"],["f","formatBytes()"],["c","LoginForm"],["c","PhpextjsControllerCode"],["c","PhpextjsControllerGenerator"],["c","PHPExtJSCrudCode"],["c","PHPExtJSCrudGenerator"],["c","SiteController"],["c","SiteTest"],["c","User"],["c","UserController"],["c","UserIdentity"],["c","WebTestCase"],["c","WebUser"]];
